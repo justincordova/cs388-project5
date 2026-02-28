6a5ad1b8e1d6339540f9b0891a56ab3286e06a35
Justin Cordova <justinavodroc@gmail.com>
2026-02-28 12:00:41 -0500

    feat: Increment database version to 2
    Update database version to support new photoUri field.

--- app/src/main/java/com/example/cs388project5/AppDatabase.kt
+++ app/src/main/java/com/example/cs388project5/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [FoodEntryEntity::class], version = 1)
+@Database(entities = [FoodEntryEntity::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun foodEntryDao(): FoodEntryDao
 
